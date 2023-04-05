a4c3f81f1d6d0a1f5ee07958af5b6dde6d32b243
DEEPANRAJ <118879053+Deepan9696@users.noreply.github.com>
2023-04-05 17:38:23 +0530

    Delete task directory

--- task/printparticulardata.js
+++
@@ -1,8 +0,0 @@
-fetch("https://restcountries.com/v3.1/all")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((country) => {
-      console.log(country.flags.svg);
-    });
-  })
-  .catch((error) => console.log(error));
